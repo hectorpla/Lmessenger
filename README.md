de86b85a242330db4c786d7b6dd730815e9c7db6
Zhanpeng Liang <hectorilles@gmail.com>
2018-06-18 23:31:34 -0700

    message handling: basic definitions

---
+++ README.md
@@ -0,0 +1,5 @@
+# Implementation Decision
+
+## server language choices
+- for better support of HBase, should Java
+- a node.js layer might be added, but does this introduce unnecessary complexity?
--- server/socket/socket.js
+++ server/socket/socket.js
@@ -5,16 +5,59 @@ var logger = {
 
 const SESSION = "TWO_PEP_SESSION";
 
+// naively maintain all sockets in a single server
+// so the this is a massive table containing many user info
+// should refer to node.js cluster support on 
+// https://socket.io/docs/using-multiple-nodes/
+const userInfo = new Map();
+
+function initSocketService(user, socket) {
+    // TODO: initialize socket
+    console.assert(!userInfo.has(user));
+    userInfo.set(user, {
+        socket: socket,
+    });
+
+    // TODO: implementation:
+    socket.on("message", (user, receiver, message) => {
+        // rough impl
+        
+        // TODO: 1. write to DB: call back wrapping the following
+
+        // 2. send to the receiver
+        // TODO: handle offline case
+        const receiverSocket = userInfo.get(receiver).socket;
+        receiverSocket.emmit("message", message, (ack) => {
+            // TODO: 3. ack to the sender
+        });
+    });
+
+    // TODO: implementation: 0. to determine the rest arguments (pagination? time ranges?)
+    socket.on("retrieval", (user, receiver, ...args) => {
+
+    })
+
+    socket.on("disconnect", function() {
+        logger.debug("a user quitted");
+    })
+}
+
+// TODO: whether do authentication or not?
+// security: a user should have access to only his bucket of info
 module.exports = function(io) {
     io.on("connection", function(socket) {
         logger.debug("a user connected");
 
-        // handlers
+        // TODO: authenticate
+        socket.on("auth", (...args) => {
+            // server-side firebase administration
+            const isAuthenticated = true;
 
-        socket.on("disconnect", function() {
-            logger.debug("a user quitted");
+            if (isAuthenticated) {
+                initSocketService(user, socket);
+            } else {
+                socket.disconnect();
+            }
         })
     });
-
-    
 }
